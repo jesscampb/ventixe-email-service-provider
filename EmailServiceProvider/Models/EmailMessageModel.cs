55cb4313d36bb1634d5d5002208fa65090790577
Jessica Campbell <jesscampb91@gmail.com>
2025-05-22 10:55:00 +0200

    Created EmailMessageModel and added functionality for sending emails in EmailService.

---
+++ EmailServiceProvider/Models/EmailMessageModel.cs
@@ -0,0 +1,9 @@
+﻿namespace EmailServiceProvider.Models;
+
+public class EmailMessageModel
+{
+    public List<string> Recipients { get; set; } = null!;
+    public string Subject { get; set; } = null!;
+    public string PlainText { get; set; } = null!;
+    public string Html { get; set; } = null!;
+}
---
+++ EmailServiceProvider/Services/EmailService.cs
@@ -0,0 +1,44 @@
+﻿using Azure;
+using EmailServiceProvider.Dtos;
+using EmailServiceProvider.Models;
+
+namespace EmailServiceProvider.Services;
+
+public interface IEmailService
+{
+    Task<EmailServiceResult> SendEmailAsync(EmailMessageModel message);
+}
+
+public class EmailService : IEmailService
+{
+    public async Task<EmailServiceResult> SendEmailAsync(EmailMessageModel message)
+    {
+        try
+        {
+            var recipients = message.Recipients.Select(email => new EmailAddress(email)).ToList();
+
+            var emailMessage = new EmailMessage(
+                senderAddress: _settings.SenderAddress,
+                content: new EmailContent(message.Subject)
+                {
+                    PlainText = message.PlainText,
+                    Html = message.Html
+                },
+                recipients: new EmailRecipients(recipients));
+
+            var response = await _client.SendAsync(WaitUntil.Completed, emailMessage);
+
+            return response.HasCompleted
+                ? new EmailServiceResult { Succeeded = true }
+                : new EmailServiceResult { Succeeded = false, Error = "The email send-confirmation could not be confirmed." };
+        }
+        catch (RequestFailedException ex)
+        {
+            return new EmailServiceResult { Succeeded = false, Error = $"An error occurred ({ex.Status}): {ex.Message}" };
+        }
+        catch (Exception ex)
+        {
+            return new EmailServiceResult { Succeeded = false, Error = $"An unexpected error occurred: {ex.Message}" };
+        }
+    }
+}

7e18f663dcbdc3667369ba296ef1e40e94eefe91
Jessica Campbell <jesscampb91@gmail.com>
2025-05-22 11:17:31 +0200

    Created AzureCommunicationSettings POCO.

---
+++ EmailServiceProvider/Models/AzureCommunicationSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace EmailServiceProvider.Models;
+
+public class AzureCommunicationSettings
+{
+    public string ConnectionString { get; set; } = default!;
+    public string SenderAddress { get; set; } = default!;
+}

d37090ae394907c439ccb138854df34431b2bbb7
Jessica Campbell <jesscampb91@gmail.com>
2025-05-22 12:12:52 +0200

    Registered EmailService and configured AzureCommunicationSettings in Program.

--- EmailServiceProvider/Program.cs
+++ EmailServiceProvider/Program.cs
@@ -1,14 +1,26 @@
+using Azure.Communication.Email;
+using EmailServiceProvider.Models;
+using EmailServiceProvider.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 
 var builder = FunctionsApplication.CreateBuilder(args);
-
-builder.ConfigureFunctionsWebApplication();
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
 
+builder.Services.Configure<AzureCommunicationSettings>(builder.Configuration.GetSection("AzureCommunication"));
+builder.Services.AddSingleton<EmailClient>(provider =>
+{
+    var settings = provider.GetRequiredService<IOptions<AzureCommunicationSettings>>().Value;
+    return new EmailClient(settings.ConnectionString);
+});
+
+builder.Services.AddSingleton<IEmailService, EmailService>();
+
+builder.ConfigureFunctionsWebApplication();
 builder.Build().Run();
